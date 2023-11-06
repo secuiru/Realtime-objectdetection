d353d3d8605b49a2c7ac4fbada5dc75c0e43b779
secuiru <vili.kauhanen@gmail.com>
2023-11-06 11:59:39 +0200

    second

---
+++ webui.py
@@ -0,0 +1,36 @@
+# import gradio package
+import gradio as gr
+from torchvision.io.image import read_image
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
+from torchvision.utils import draw_bounding_boxes
+from torchvision.transforms.functional import to_pil_image
+import numpy as np
+
+def object_detect(input_img):
+    img = read_image(input_img)
+
+    # Step 1: Initialize model with the best available weights
+    weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+    model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.8)
+    model.eval()
+
+    # Step 2: Initialize the inference transforms
+    preprocess = weights.transforms()
+
+    # Step 3: Apply inference preprocessing transforms
+    batch = [preprocess(img)]
+
+    # Step 4: Use the model and visualize the prediction
+    prediction = model(batch)[0]
+    labels = [weights.meta["categories"][i] for i in prediction["labels"]]
+    box = draw_bounding_boxes(img, boxes=prediction["boxes"],
+                            labels=labels,
+                            colors="red",
+                            width=4, font_size=30)
+    im = to_pil_image(box.detach())
+
+    #im.show()
+    return im
+    # launch the app!
+demo = gr.Interface(object_detect,gr.Image(type="filepath"),"image")
+demo.launch()
